d2392bc00934400b6686fdc288deb02b22af7339
Florin Pantelimon <florin.pantelimon@yahoo.com>
2020-10-17 14:01:09 +0300

    Added examples

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(express.json())
+
+var studentsList = [
+    {
+        id: 1,
+        name: 'Andrei',
+        age: 22,
+        studyGroup:{
+            name:'1078',
+            year: 3
+        }
+    },
+    {
+        id:2,
+        name:'Ana',
+        age:22,
+        studyGroup: {
+            name:'1078',
+            year:3
+        }
+    },
+    {
+        id:3,
+        name:'Alexandru',
+        age:22,
+        studyGroup: {
+            name:'1078',
+            year:3
+        }
+    }
+]
+
+app.get('/', (request,response)=>{
+    response.send("App works!")
+})
+
+app.get('/api/students', (req,res) => {
+    res.status(200)
+    res.send(studentsList)
+})
+
+app.get('/api/students/:id', (req,res)=>{
+    res.status(200)
+    res.send(studentsList.filter(x=>x.id == req.params.id))
+})
+
+app.delete('/api/students/:id', (req,res)=>{
+    studentsList = studentsList.filter(x=>x.id != req.params.id)
+    res.status(200)
+    res.send(studentsList)
+})
+
+app.put('/api/students/:id', (req,res)=>{
+    studentsList = studentsList.filter(x=>x.id != req.params.id)
+    studentsList.push(req.body)
+    res.status(200)
+    res.send(studentsList)
+})
+
+app.post('/api/students/add', (req,res)=>{
+    studentsList.push(req.body)
+    res.status(200)
+    res.send(studentsList)
+})
+
+app.delete('/api/students/:id')
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+})
